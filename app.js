3f7dc04e4c3193d328a04071269534a7d2cc92cd
karina ivanova <86211129+karina4840@users.noreply.github.com>
2021-11-26 10:51:00 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+//light share button next to the name
+const shareButtonLight = document.querySelector('.share-button'); 
+const sharePopupSmall = document.querySelector('.share-popup-small');
+const sharePopupBig = document.querySelector('.share-popup-big');
+const body = document.querySelector('body');
+var width = window.innerWidth
+||  document.documentElement.clientWidth
+||  document.body.clientWidth;
+
+
+sharePopupSmall.classList.add('hidden-small')
+sharePopupBig.classList.add('hidden-big')
+
+shareButtonLight.addEventListener('click', function(){
+    if (window.innerWidth > 425) {
+        sharePopupBig.classList.remove('hidden-big')
+        body.addEventListener('mouseup', function () {
+            sharePopupBig.classList.add('hidden-big')
+        });
+    } 
+    if (window.innerWidth <= 425) {
+        sharePopupSmall.classList.remove('hidden-small')
+        body.addEventListener('mouseup', function () {
+            sharePopupSmall.classList.add('hidden-small')
+        });
+    }
+});
+
+    
+
+
+
